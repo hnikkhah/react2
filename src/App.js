c2d724d665a62c7b430a2ba696fca62da18f237c
hajar <hnikkhah81.hn@gmail.com>
2023-09-08 23:27:54 +0430

    timer

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import  Hello from './Hello'
 import Timer from './Timer'
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      title:"سلام"
+    }
+  }
+  handleTitle=()=>{
+    this.setState({
+      title:"خوش آمدید"
+    })
+  }
     render(){
     return(
-      <div className="main"><Hello/><Timer/></div>
+      <div className="main">
+        <Hello title={this.state.title}/>
+        <Timer handleTitle={this.handleTitle}/></div>
       )}
      }
      export default App;
--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './style.css'
 
 var interval;
@@ -7,29 +7,76 @@ class Timer extends React.Component{
     constructor(){
       super();
       this.state={
-        time : 10
-      }
-    }
-    componentDidMount(){
-        
-       interval= setInterval(() => {
-            this.setState({
-            time : this.state.time-1
-           })}, 1000)
-    }
-    componentDidUpdate(){
-      if(this.state.time==0){
-        clearInterval(interval);
-      }
-    }
-  render(){
-    
-    return(  
-     <h2 className="timer">
-      it is {this.state.time}
-     </h2>
-    
-     )}
-   }
+        hour:0,
+        minute:0,
+        second:0,
+        isStart:false
 
+      }
+    }
+  
+    start=() =>{
+     if(this.state.isStart==false) {
+      this.setState({
+        isStart:true
+      })
+      
+      interval= setInterval(() => {
+            this.setState({
+           second : this.state.second+1
+
+          })
+          if( this.state.second==60){
+          this.setState({
+            second:0,
+            minute: this.state.minute +1
+          })
+          }
+         if(this.state.minute==60)
+         {this.setState({
+         minute:0,
+         hour: this.state.hour+1
+         })
+          
+         }
+          } , 1000)
+    }
+  }
+
+    stop=() =>{
+      this.setState({
+        isStart:false})
+        clearInterval(interval);
+       
+      }
+
+     reset=() =>{
+     
+       this.stop();
+       this.setState({
+      hour:0,
+      minute:0,
+      second:0,
+    })
+  }
+
+  render(){
+    let s=this.state.second;
+    let m=this.state.minute;
+    let h=this.state.hour;
+    return(  
+      <>
+     <h2 className="timer">
+      {`${h>9?h:"0"+h}:${m>9?m:"0"+m}:${s>9?s:"0"+s}` }
+     </h2>
+ <div className="button_box">
+  <span className="action-button" onClick={this.start}>start</span>
+  <span className="action-button" onClick={this.stop}>stop</span>
+  <span className="action-button" onClick={this.reset}>reset</span>
+
+ </div>
+ </>
+     )}
+  
+    }
    export default Timer;
